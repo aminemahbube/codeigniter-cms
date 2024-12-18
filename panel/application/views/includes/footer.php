28616fb46d32552948ca5362b60a4363c4282d5a
Amine Sonmez <aminemsonmez@gmail.com>
2024-12-18 17:54:20 +0300

    Dashboard_v icin bir klasor olusturuldu. Bos content sayfasi yapildi. Tema parcalama-entegre islemleri yapildi.

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,10 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+      <div class="clearfix">
+        <ul class="footer-menu pull-right">
+          <li><a href="javascript:void(0)">Privacy Policy</a></li>
+        </ul>
+        <div class="copyright pull-left"> &copy; Tüm hakları saklıdır. |  Aydın - <?php echo date("Y"); ?> </div>
+      </div>
+    </footer>
+  </div>
