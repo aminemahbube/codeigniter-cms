e4d9b3370f516d3f95a83d24d6f60cfd0a405d73
Amine Sonmez <aminemsonmez@gmail.com>
2024-12-20 13:51:13 +0300

    Tema renk özellestirmesi basarılı. Right aside silindi. Navbar düzenlendi.

--- panel/assets/assets/js/app.js
+++ panel/assets/assets/js/app.js
@@ -552,11 +552,21 @@
 }(jQuery, window);
 
 // initialize app
-+function($, window) { 'use strict';
-	window.app.init();
-	window.app.menubar.init();
-	window.app.navbar.init();
-	window.app.customizer.init();
++function ($, window) {
+    'use strict';
+    window.app.init();
+
+    window.app.menubar.setTheme("dark");
+    window.app.menubar.applyTheme();
+
+    window.app.navbar.setTheme("danger");
+    window.app.navbar.applyTheme();
+
+    window.app.saveSettings();
+
+    window.app.menubar.init();
+    window.app.navbar.init();
+    window.app.customizer.init();
 }(jQuery, window);
 
 // other
