a1f5edb6d3f47349eb5c130317eceb8d02cae1d7
Amine Sonmez <aminemsonmez@gmail.com>
2024-12-22 13:59:54 +0300

    viewFolder ve subViewFolder yapisi getirildi. View klasorleri list, add, update olarak üce bolundu. Urunler listelemesinin view yapisi icin giris olusturuldu.

--- panel/application/views/includes/navbar.php
+++ panel/application/views/includes/navbar.php
@@ -42,7 +42,7 @@
             <i class="zmdi zmdi-hc-lg zmdi-search"></i>
           </a>
         </li>
-
+        
       </ul>
     </div>
   </div><!-- navbar-container -->
---
+++ panel/application/views/product_v/list/content.php
@@ -0,0 +1,1 @@
+Urun listesi content..
