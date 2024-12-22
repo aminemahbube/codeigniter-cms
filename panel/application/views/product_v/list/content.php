0b2f3ce85c5e45e68b220a7b6a67215dd7363683
Amine Sonmez <aminemsonmez@gmail.com>
2024-12-22 15:40:09 +0300

    Urunler modulunun urun listesi icin html sablonu olusturuldu.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,32 @@
-Urun listesi content..
+<div class="row">
+<div class="col-md-12">
+<h4 class="m-b-lg">Ürün Listesi</h4>
+<div class="col-md-12">
+				<div class="widget p-lg">
+					<table class="table table-hover table-striped">
+						<thead>
+                            <th>#id</th>
+                            <th>url</th>
+                            <th>Başlık</th>
+                            <th>Açıklama</th>
+                            <th>Durumu</th>
+                            <th>İşlem</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>#1</td>
+                                <td>monitor-askisi</td>
+                                <td>Monitör Askısı</td>
+                                <td>360 derece kullanılabilen monitör askısıdır.</td>
+                                <td>1/0</td>
+                                <td>
+                                <a href="#" class="btn btn-primary btn-sm btn-outline me-2"><i class="fa fa-pencil-square"></i> Düzenle</a>
+								<a href="#" class="btn btn-purple btn-sm btn-outline me-2"><i class="fa fa-trash"></i> Sil</a>
+                                </td>
+                            </tr>
+                          
+                        </tbody>
+					</table>
+				</div><!-- .widget -->
+			</div><!-- END column -->
+</div>
