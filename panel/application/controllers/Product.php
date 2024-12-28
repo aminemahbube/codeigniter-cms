3adb93093cda7a61e5db2332c91906fc9e5f266a
Amine Sonmez <aminemsonmez@gmail.com>
2024-12-28 16:59:27 +0300

    Urun Ekle formu icin form_validation entegre edildi.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -41,7 +41,31 @@ class Product extends CI_Controller
     }
 
     public function save(){
-        echo "saved";
+       
+        $this->load->library("form_validation");
+
+        //Kurallar yazılır.
+        $this->form_validation->set_rules("title","Başlık","required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required" => "<b>{field}</b> alanı doldurulmalıdır."
+            )
+        );
+
+        //Form Validation Çalıştırılır
+        //True-False
+        $validate = $this->form_validation->run();
+
+        if($validate){
+            echo "Kayıt işlemi başlar...";
+        } else {
+            echo validation_errors();
+        }
+        //Başarılı ise 
+            //Kayıt işlemi başlar
+        //Başarısız ise
+            //Hata ekranda gösterilir.
     }
 
     
