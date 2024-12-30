092ca8eff898c4ec5a3594ccef1c8043eb0934b2
Amine Sonmez <aminemsonmez@gmail.com>
2024-12-30 12:20:16 +0300

    product_model icerisinde add() metodu tanimlandi. Verilerin tabloya kayit edilmesi(veritabanına) gerceklestirildi.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -58,7 +58,27 @@ class Product extends CI_Controller
         $validate = $this->form_validation->run();
 
         if($validate){
-            echo "Kayıt işlemi başlar...";
+
+            $insert = $this->product_model->add(
+                array(
+                    "title"     => $this->input->post("title"),
+                    "description" => $this->input->post("description"),
+                    "url"         => "test...",
+                    
+                    "isActive"    => 1,
+                    "createdAt"   => date("Y-m-d H:i:s")
+                )
+            );
+            if($insert){
+
+                echo "Kayıt işlemi başarılıdır.";
+                
+            }else {
+
+                echo "İşlem başarısızdır.";
+
+            }
+
         } else {
             $viewData=new stdClass();
 
