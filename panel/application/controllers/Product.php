1dd0a28be0693da420a3680aa63ca53e211faf78
Amine Sonmez <aminemsonmez@gmail.com>
2025-01-05 18:13:41 +0300

    upload isleminin tabloya aktarilmasi(veritabani)

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -11,6 +11,8 @@ class Product extends CI_Controller
         $this->viewFolder = "product_v";
 
         $this->load->model('product_model');
+        $this->load->model('product_image_model');
+
     }
 
     public function index(){
@@ -262,7 +264,7 @@ class Product extends CI_Controller
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
 
-    public function image_upload(){
+    public function image_upload($id){
 
         $config["allowed_types"] = "jpg|jpeg|png";
         $config["upload_path"]   = "uploads/$this->viewFolder/";
@@ -271,7 +273,20 @@ class Product extends CI_Controller
 
         $upload = $this->upload->do_upload("file");
         if($upload){
-            echo "islem basarili";
+            
+            $uploaded_file = $this->upload->data("file_name");
+
+            $this->product_image_model->add(
+                array(
+                    "img_url"       => $uploaded_file,
+                    "rank"          => 0,
+                    "isActive"      => 1,
+                    "isCover"       => 0,
+                    "createdAt"     => date("Y-m-d H:i:s"),
+                    "product_id"    => $id
+                )
+            );
+
         }else {
             echo "islem basarisiz";
         }
