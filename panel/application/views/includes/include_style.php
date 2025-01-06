0434010f8c23721445a2409603fb7616ff72e74e
Amine Sonmez <aminemsonmez@gmail.com>
2025-01-06 17:46:55 +0300

    iziToast eklentisi yuklendi.

--- panel/application/views/includes/include_style.php
+++ panel/application/views/includes/include_style.php
@@ -11,6 +11,8 @@
 	<!-- endbuild -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900,300">
 
+
+	<link rel="stylesheet" href="<?php echo base_url("assets"); ?>/assets/css/iziToast.min.css">
 	<link rel="stylesheet" href="<?php echo base_url("assets"); ?>/assets/css/custom.css">
 	<link rel="stylesheet" href="<?php echo base_url("assets"); ?>/assets/css/dropzone.min.css">
 	<script src="<?php echo base_url("assets"); ?>/libs/bower/breakpoints.js/dist/breakpoints.min.js"></script>
