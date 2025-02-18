c7d89415d54f106cc6e5e8fd110d7fc088373a19
Amine Sonmez <aminemsonmez@gmail.com>
2025-02-18 11:39:30 +0300

    Haberler modulunun olusturulmasi ve haberlerin listelenmesinin yapilamasi veritabani baglantisi olusturuldu kisaca.

---
+++ panel/application/views/news_v/list/content.php
@@ -0,0 +1,73 @@
+<div class="row">
+<div class="col-md-12">
+<h4 class="m-b-lg">
+    Haber Listesi
+    <a href="<?php echo base_url("news/new_form"); ?>" class="btn pull-right btn-outline btn-info btn-xs"><i class="fa fa-plus"></i> Yeni Ekle</a>
+</h4>
+</div>
+<div class="col-md-12">
+			<div class="widget p-lg">
+
+                <?php if(empty($items)) { ?>
+
+                <div class="alert alert-warning text-center">
+								<h5 class="alert-title">Kayıt Bulunamadı</h5>
+								<p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="<?php echo base_url("news/new_form"); ?>">tıklayınız.</a> </p>
+							</div>
+                    
+                <?php } else { ?>        
+
+					<table class="table table-hover table-striped table-bordered content-container">
+						<thead>
+                            <th class="order"><i class="fa fa-reorder"></i></th>
+                            <th class="w50">#id</th>
+                            <th>Başlık</th>
+                            <th>url</th>
+                            <th>Açıklama</th>
+                            <th>Haber Türü</th>
+                            <th>Görsel</th>
+                            <th>Durumu</th>
+                            <th>İşlem</th>
+                        </thead>
+                        <tbody class="sortable" data-url="<?php echo base_url("news/rankSetter"); ?>">
+                        
+                            <?php foreach($items as $item) { ?>
+                            
+                                <tr id="ord-<?php echo $item->id; ?>">
+                                <td class="order"><i class="fa fa-reorder"></i></td>
+                                <td class="w50 text-center">#<?php echo $item->id; ?></td>
+                                <td class="w100 text-center"><?php echo $item->title; ?></td>
+                                <td><?php echo $item->url; ?></td>
+                                <td><?php echo $item->description; ?></td>
+                                <td><?php echo $item->news_type; ?></td>
+                                <td>Görsel Gelecek</td>
+                                <td>
+                                    <input 
+                                    data-url="<?php echo base_url("news/isActiveSetter/$item->id"); ?>"
+                                    class="isActive"
+                                    type="checkbox" 
+                                    data-switchery 
+                                    data-color="#10c469"  
+                                    <?php echo ($item->isActive) ? "checked" : ""; ?>
+                                    />
+							    </td>
+                                <td class="order">
+                                    <a href="<?php echo base_url("news/update_form/$item->id"); ?>" class="btn btn-primary btn-sm btn-outline "><i class="fa fa-pencil-square"></i> Düzenle</a>
+								<button 
+                                    data-url="<?php echo base_url("news/delete/$item->id"); ?>"
+                                    class="btn btn-danger btn-sm btn-outline remove-btn">
+                                    <i class="fa fa-trash"></i> Sil
+                                </button>
+                                </td>
+                            </tr>
+                    
+
+                            <?php } ?>
+                        </tbody>
+					</table>
+                
+                <?php } ?>    
+
+				</div><!-- .widget -->
+			</div><!-- END column -->
+</div>
