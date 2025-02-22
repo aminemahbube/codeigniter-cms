9706572b211afcddb941ccf968fdeb8978968e5e
Amine Sonmez <aminemsonmez@gmail.com>
2025-02-22 20:50:45 +0300

    haberler modulunun listeleme viewi duzenlendi modul bitti

--- panel/application/views/news_v/list/content.php
+++ panel/application/views/news_v/list/content.php
@@ -23,7 +23,7 @@
                             <th class="w50">#id</th>
                             <th>Başlık</th>
                             <th>url</th>
-                            <th>Açıklama</th>
+                            <!-- <th>Açıklama</th> -->
                             <th>Haber Türü</th>
                             <th>Görsel</th>
                             <th>Durumu</th>
@@ -38,28 +38,29 @@
                                 <td class="w50 text-center">#<?php echo $item->id; ?></td>
                                 <td class="w100 text-center"><?php echo $item->title; ?></td>
                                 <td><?php echo $item->url; ?></td>
-                                <td><?php echo $item->description; ?></td>
-                                <td><?php echo $item->news_type; ?></td>
+                                <!-- <td><?php //echo $item->description; ?></td> -->
+                                <td class="w50 text-center"><?php echo $item->news_type; ?></td>
                                 <td>
                                     <?php if($item->news_type == "image") { ?>
                                         <div style="text-align: center;">
-                                        <img width="100" src="<?php echo base_url("uploads/$viewFolder/$item->img_url"); ?>"
+                                        <img width="75" src="<?php echo base_url("uploads/$viewFolder/$item->img_url"); ?>"
                                         alt=""
                                         class="img-rounded">
                                     
                                     <?php } if ($item->news_type == "video") { ?>
-
+                                    <center>
                                     <iframe
-                                        height="150"
+                                        height="75" width="225"
                                         src="<?php echo $item->video_url; ?>"
                                         gesture="media"
                                         allow="encrypted-media"
                                         allowfullscreen>
                                     </iframe>
-                                    
+                                    </center>
                                     <?php } ?>
                                 </td>
                                 <td>
+                                    <center>
                                     <input 
                                     data-url="<?php echo base_url("news/isActiveSetter/$item->id"); ?>"
                                     class="isActive"
@@ -68,7 +69,7 @@
                                     data-color="#10c469"  
                                     <?php echo ($item->isActive) ? "checked" : ""; ?>
                                     />
-							    </td>
+							    </td></center>
                                 <td class="order">
                                     <a href="<?php echo base_url("news/update_form/$item->id"); ?>" class="btn btn-primary btn-sm btn-outline "><i class="fa fa-pencil-square"></i> Düzenle</a>
 								<button 
