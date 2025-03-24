df4f00ff81d7d28735f17071b6514491f6000cad
Amine Sonmez <236029028@stu.adu.edu.tr>
2025-03-24 12:52:06 +0300

    Galeri modulu islemleri yapiliyor.

--- panel/application/views/galleries_v/list/content.php
+++ panel/application/views/galleries_v/list/content.php
@@ -1,8 +1,8 @@
 <div class="row">
 <div class="col-md-12">
 <h4 class="m-b-lg">
-    Marka Listesi
-    <a href="<?php echo base_url("brands/new_form"); ?>" class="btn pull-right btn-outline btn-info btn-xs"><i class="fa fa-plus"></i> Yeni Ekle</a>
+    Galeri Listesi
+    <a href="<?php echo base_url("galleries/new_form"); ?>" class="btn pull-right btn-outline btn-info btn-xs"><i class="fa fa-plus"></i> Yeni Ekle</a>
 </h4>
 </div>
 <div class="col-md-12">
@@ -12,38 +12,36 @@
 
                 <div class="alert alert-warning text-center">
 								<h5 class="alert-title">Kayıt Bulunamadı</h5>
-								<p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="<?php echo base_url("brands/new_form"); ?>">tıklayınız.</a> </p>
+								<p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="<?php echo base_url("galleries/new_form"); ?>">tıklayınız.</a> </p>
 							</div>
                     
                 <?php } else { ?>        
 
 					<table class="table table-hover table-striped table-bordered content-container">
 						<thead>
-                            <th class="w50 order"><i class="fa fa-reorder"></i></th>
+                            <th class="order"><i class="fa fa-reorder"></i></th>
                             <th class="w50">#id</th>
-                            <th>Başlık</th>
-                            <!-- <<th>Açıklama</th> -->
-                            <th>Görsel</th>
+                            <th>Galeri Adı</th>
+                            <th>Galeri Türü</th>
+                            <th>Klasör Adı</th>
+                            <th>url</th>
                             <th>Durumu</th>
                             <th>İşlem</th>
                         </thead>
-                        <tbody class="sortable" data-url="<?php echo base_url("brands/rankSetter"); ?>">
+                        <tbody class="sortable" data-url="<?php echo base_url("galleries/rankSetter"); ?>">
                         
                             <?php foreach($items as $item) { ?>
                             
                                 <tr id="ord-<?php echo $item->id; ?>">
                                 <td class="order"><i class="fa fa-reorder"></i></td>
                                 <td class="w50 text-center">#<?php echo $item->id; ?></td>
-                                <td class="text-center"><?php echo $item->title; ?></td>
-                                <!-- <td><?php //echo $item->description; ?></td> -->
-                                <td >
-                                    <div style="text-align: center;">
-                                    <img width="75" src="<?php echo base_url("uploads/$viewFolder/$item->img_url"); ?>"
-                                        alt="" class="img-rounded">
-                                </td>
-                                <td class="text-center w100">
+                                <td class="w100 text-center"><?php echo $item->title; ?></td>
+                                <td class="w100 text-center"><?php echo $item->gallery_type; ?></td>
+                                <td class="w100 text-center"><?php echo $item->folder_name; ?></td>
+                                <td><?php echo $item->url; ?></td>
+                                <td class="text-center">
                                     <input 
-                                    data-url="<?php echo base_url("brands/isActiveSetter/$item->id"); ?>"
+                                    data-url="<?php echo base_url("galleries/isActiveSetter/$item->id"); ?>"
                                     class="isActive"
                                     type="checkbox" 
                                     data-switchery 
@@ -51,13 +49,14 @@
                                     <?php echo ($item->isActive) ? "checked" : ""; ?>
                                     />
 							    </td>
-                                <td class="w200 text-center">
-                                    <a href="<?php echo base_url("brands/update_form/$item->id"); ?>" class="btn btn-primary btn-sm btn-outline "><i class="fa fa-pencil-square"></i> Düzenle</a>
+                                <td class="order">
+                                    <a href="<?php echo base_url("galleries/update_form/$item->id"); ?>" class="btn btn-primary btn-sm btn-outline "><i class="fa fa-pencil-square"></i> Düzenle</a>
 								<button 
-                                    data-url="<?php echo base_url("brands/delete/$item->id"); ?>"
+                                    data-url="<?php echo base_url("galleries/delete/$item->id"); ?>"
                                     class="btn btn-danger btn-sm btn-outline remove-btn">
                                     <i class="fa fa-trash"></i> Sil
                                 </button>
+                                <a href="<?php echo base_url("galleries/image_form/$item->id"); ?>" class="btn btn-dark btn-sm btn-outline "><i class="fa fa-image"></i> Resimler</a>
                                 </td>
                             </tr>
                     
